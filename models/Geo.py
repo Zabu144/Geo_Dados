9076b4108d271e7d8ca42d5e8a3806b289ab3ea5
Luis Eduardo Muniz Lopes <101485625+Zabu144@users.noreply.github.com>
2023-11-01 14:50:55 +0000

    Add `Geo.py` class

---
+++ models/Geo.py
@@ -0,0 +1,10 @@
+# pylint: disable=all
+
+class Geo:
+  def __init__(self, id, numOriginal, furo, geometalurgico, status, tipoMaterial):
+    self.id = id
+    self.numOriginal = numOriginal
+    self.furo = furo
+    self.geometalurgico = geometalurgico
+    self.status = status
+    self.tipoMaterial = tipoMaterial
