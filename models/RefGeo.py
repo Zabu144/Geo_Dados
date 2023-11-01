6c4b7541ad20f12c10a6c49d30ee4b9aa674ba0b
Luis Eduardo Muniz Lopes <101485625+Zabu144@users.noreply.github.com>
2023-11-01 14:56:07 +0000

    Add `RefGeo.py` class

---
+++ models/RefGeo.py
@@ -0,0 +1,13 @@
+# pylint: disable=all
+
+class RefGeo:
+  def __init__(self, id, location_i, status, location_x, location_y, location_z, fase, banco, poligono):
+    self.id = id
+    self.location_i = location_i
+    self.status = status
+    self.location_x = location_x
+    self.location_y = location_y
+    self.location_z = location_z
+    self.fase = fase
+    self.banco = banco
+    self.poligono = poligono
